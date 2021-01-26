d5fbaf476feb9aaf0b9d84f6fbcbe7cf9ce2224a
Chad <40812274+MrWittman612@users.noreply.github.com>
2021-01-26 17:59:58 -0600

    changeing logo more init stuff

--- .storybook/CustomStoryBookTheme.js
+++ .storybook/CustomStoryBookTheme.js
@@ -1,9 +1,9 @@
-import { create } from '@storybook/theming';
-import image from '../src/assets/blog-logo.png';
+import { create } from "@storybook/theming";
+import image from "../src/assets/my-mui-storybook-logo.png";
 
 export default create({
-  base: 'dark',
-  brandTitle: 'Mr. Wittman Blog',
-  brandUrl: 'https://mrwittman-blog-crm.herokuapp.com/',
+  base: "dark",
+  brandTitle: "Mr. Wittman Blog",
+  brandUrl: "https://mrwittman-blog-crm.herokuapp.com/",
   brandImage: image,
 });
