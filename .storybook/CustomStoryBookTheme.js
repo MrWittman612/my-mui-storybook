4a502f5685388e2d4757f44d7b599e615ec1f04a
Chad <40812274+MrWittman612@users.noreply.github.com>
2021-01-26 18:12:30 -0600

    edit brandURL

--- .storybook/CustomStoryBookTheme.js
+++ .storybook/CustomStoryBookTheme.js
@@ -4,6 +4,7 @@ import image from "../src/assets/my-mui-storybook-logo.png";
 export default create({
   base: "dark",
   brandTitle: "Mr. Wittman Blog",
-  brandUrl: "https://mrwittman-blog-crm.herokuapp.com/",
+  brandUrl:
+    "https://mrwittman612.github.io/my-mui-storybook/?path=/story/example-introduction--page",
   brandImage: image,
 });
